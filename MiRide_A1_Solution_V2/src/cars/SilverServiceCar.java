67dfa139dfc46a0fa79cc534a12b18154c85ed7b
frisb <frisb@192.168.0.18>
2019-05-15 16:39:08 +1000

    Creation of the sub-class Override booking method for silver service car Started updating the getDetails and toString method

--- MiRide_A1_Solution_V2/src/cars/SilverServiceCar.java
+++ MiRide_A1_Solution_V2/src/cars/SilverServiceCar.java
@@ -5,47 +5,65 @@ import utilities.DateTime;
 public class SilverServiceCar extends Car {
 	private String[] refreshments;
 	private double bookingFee;
+	private boolean available;
+	
 
 	public SilverServiceCar(String regNo, String make, String model, String driverName, int passengerCapacity, double bookingFee, String[] refreshments) {
 		super(regNo,make,model,driverName,passengerCapacity);
 		this.bookingFee = bookingFee;
 		this.refreshments = refreshments;
+		available = true;
 		
 		//Booking Fee has to be greater than or equal to $3.00
 		if (bookingFee < 3.00) {
-			System.out.println("Error: Booking Fee Too Small");
+			bookingFee = 3.00;
 		}
 		
 		
 	}
 	
-	public boolean book(String firstName, String lastName, DateTime required, int numPassengers)
-	{
+	@Override
+	public boolean book(String firstName, String lastName, DateTime required, int numPassengers) {
 		boolean booked = false;
+		DateTime currentDate = new DateTime();
 		
-		
-		// Does car have five bookings
-		available = bookingAvailable();
-		boolean dateAvailable = notCurrentlyBookedOnDate(required);
-		// Date is within range, not in past and within the next week
-		boolean dateValid = dateIsValid(required);
-		boolean validPassengerNumber = numberOfPassengersIsValid(numPassengers);
-
-		// Booking is permissible
-		if (available && dateAvailable && dateValid && validPassengerNumber)
-		{
-			tripFee = STANDARD_BOOKING_FEE;
-			Booking booking = new Booking(firstName, lastName, required, numPassengers, this);
-			currentBookings[bookingSpotAvailable] = booking;
-			bookingSpotAvailable++;
-			booked = true;
+		//Check if date is less than or equal to 3 days in future
+		//Parent booking method checks for valid date
+		if (DateTime.diffDays(required, currentDate) <= 3) {
+			booked = super.book(firstName, lastName, required, numPassengers);
+			return booked;
 		}
+		
 		return booked;
 	}
 	
+	@Override
 	public String getDetails() {
-		super.getDetails();
+		int count = refreshments.length;
 		
+		String details = super.getDetails()
+			+ String.format("%s\n", "")
+			+ String.format("%s\n","Refreshments Available");
+		for (int i = 0; i < count; i++) {
+			details = details + String.format("%-25s %s\n", "Item " + (i+1),refreshments[i]);
+		}
+		
+		System.out.println(details);
+		return details;
+		
+	}
+	
+	@Override
+	public String toString() {
+		int count = refreshments.length;
+		
+		String toString = super.toString();
+		for (int i = 0; i < count; i++) {
+			toString = toString + String.format(":" + "Item " + (i+1) + " " + refreshments[i]);
+		}
+		
+		
+		return toString;
 	}
 	
 }
